06c84f448d8685a537d5a18c1978994179ae66d9
carlosworld <carlosworld93@gmail.com>
2020-08-13 08:01:51 -0500

    se realizo los cambios la insercion de usuario del loguin y la lectura de los mismos

---
+++ inc/modelos/modelo-admin.php
@@ -0,0 +1,89 @@
+<?php
+
+$accion = (isset($_REQUEST['accion'])) ? $_REQUEST['accion'] : '';
+$password = $_POST['password'];
+$usuario = $_POST['usuario'];
+
+    if($accion == 'crear') {
+        // Codifo para crear los administradores.
+
+        //hashear passwords.
+        $opciones = array(
+            'cost' => 12
+        );
+
+        $hash_password = password_hash($password, PASSWORD_BCRYPT, $opciones);
+        // Importar la conexión.
+        include '../funciones/conexion.php';
+
+        try {
+            // Realizar la caonsulta a la base de datos.
+            $stmt = $conn->prepare("INSERT INTO usuarios (usuario, password) VALUES (?,?)");
+            $stmt->bind_param('ss',$usuario, $hash_password);
+            $stmt->execute();
+            if ($stmt->affected_rows) {
+                $respuesta = array(
+                    'respuesta' => 'correcto',
+                    'id_insertado'=> $stmt->insert_id,
+                    'tipo' => $accion
+                );
+            }else{
+                $respuesta = array(
+                    'respuesta' => 'error',
+
+                );
+            }
+
+            $stmt->close();
+            $conn->close();
+        } catch (Exception $e) {
+            // En caso de un error, tomar la conexión.
+            $respuesta = array(
+                'pass' => $e->getMessage()
+            );
+        }
+
+
+
+        echo json_encode($respuesta);
+    }
+
+    else if($accion == 'login'){
+
+
+        // Escribir codigo para que loguee los administradores
+        include '../funciones/conexion.php';
+
+        try {
+            // Realizar la caonsulta a la base de datos.
+            $stmt = $conn->prepare("SELECT usuario, id, password FROM usuarios WHERE usuario = ?");
+            $stmt->bind_param('s', $usuario);
+            $stmt->execute();
+            // Loguear el usuario.
+            $stmt->bind_result($nombre_usuario, $id_usuario, $pass_usuario);
+            $stmt->fetch();
+
+            if ($nombre_usuario) {
+                $respuesta = array(
+                    'respuesta' => 'correcto',
+                    'nombre'=> $nombre_usuario,
+                    'id' => $id_usuario,
+                    'pass' => $pass_usuario
+                );
+            }else{
+                $respuesta = array(
+                    'respuesta' => 'Usuario no existe!'
+
+                );
+            }
+
+            $stmt->close();
+            $conn->close();
+        } catch (Exception $e) {
+            // En caso de un error, tomar la conexión.
+            $respuesta = array(
+                'pass' => $e->getMessage()
+            );
+        }
+        echo json_encode($respuesta);
+    }
--- js/formulario.js
+++ js/formulario.js
@@ -6,23 +6,69 @@ function eventListeners() {
 
 function validarRegistro(e){
   e.preventDefault();
-  
+
  var usuario = document.querySelector('#usuario').value,
-     password = document.querySelector('#password').value;
+     password = document.querySelector('#password').value,
+     tipo = document.querySelector('#tipo').value;
 
      if (usuario === ''|| password === '') {
-      
+      // La validacion Fallo
+
       Swal.fire({
         type: 'error',
         title: 'Error!',
         text: 'Ambos campos son obligatorios!'
       })
-       
+
      }else{
+      //  Ambos campos son correctos, mandar ejecutar Ajax
+
+      // Datos que se envian al servidor
+      var datos = new FormData();
+      datos.append('usuario', usuario);
+      datos.append('password', password);
+      datos.append('accion', tipo);
+
+      // Crear el llamado ajax
+
+      var xhr = new XMLHttpRequest();
+
+      // Abrir la conexión.
+      xhr.open('POST','inc/modelos/modelo-admin.php', true);
+
+      // Retorno de datos
+
+      xhr.onload = function() {
+        if(this.status === 200){
+          // console.log(JSON.parse(xhr.responseText));
+
+          var respuesta = JSON.parse(xhr.responseText);
+console.log(respuesta);
+          if (respuesta.respuesta === 'correcto') {
+            if (respuesta.tipo === 'crear') {
+              Swal.fire({
+                type: 'success',
+                title: 'corrrecto!',
+                text: 'Agregastes correctamente los campos!'
+          });
+        }
+        // }else if (respuesta.tipo === 'login') {
+        //
+        // }
+    }
+    else{
       Swal.fire({
-        type: 'success',
-        title: 'corrrecto!',
-        text: 'Agregastes correctamente los campos!'
-      })
-     }
+        type: 'error',
+        title: 'error!',
+        text: 'hubo un error!'
+  });
+    }
+  }
 }
+      // Enviar la peticion.
+
+      xhr.send(datos);
+
+
+     }
+  }
